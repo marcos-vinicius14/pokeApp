62d19248e145172e046279726077e2fa099bee90
marcosvinicius <marcos.vinicius.dev3@gmail.com>
2025-06-20 09:45:13 -0300

    feat: create integration tests to pokeApi.service.ts

---
+++ src/setup-jest.ts
@@ -0,0 +1,5 @@
+// Importa a função recomendada para configurar o ambiente de testes do Zone.js
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+// Executa a função
+setupZoneTestEnv();
